8711b8d6bf37fea746db7ef3aa7e2499e433f6d4
BrielleCarvalho <brielle.127@gmail.com>
2019-02-06 19:34:44 -0600

    ajax working

--- assets/js/musixmusic.js
+++ assets/js/musixmusic.js
@@ -7,20 +7,26 @@ $(document).ready(function() {
 // var title = "fantasy";
 var queryURL = "https://api.musixmatch.com/ws/1.1/matcher.track.get?format=jsonp&callback=callback&q_track=freebird&apikey=8d500052733636b90bdda68c9ce64e78";
 
+
+
+$.ajax({
+    datatype: "json",
+    url: queryURL,
+    success: console.log("WORKING")
+}).then(function(response){
+    console.log(response);
+});
+
 // $.getJSON({
 //     url: queryURL,
 //     method:"GET"
 // }).then(function(response){
-//     console.log(response);
-    
+//     console.log(response);  
 // })
 
-$.getJSON({
-    url: queryURL,
-    method:"GET"
-}, function(response) {
-    console.log(response);
-});
+// $.getJSON(queryURL, function(response) {
+//     console.log(response);
+// });
 
 
 });
